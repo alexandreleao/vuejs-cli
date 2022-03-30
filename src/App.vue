383ef7a2414c75049bf6f033d194bd9ba1fb6f57
alexandreleao <erdleosalgueiro@gmail.com>
2022-03-30 15:01:52 -0300

    Primeiro Commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Comments/>
+    <FormTodo/>
+  </div>
+  
+</template>
+
+<script>
+import Comments from './components/Comments'
+
+export default {
+  name: 'app',
+  components: {
+    Comments,
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
